7fd5e1645c8b8600540894133c1ae2e6beec4aaf
Ishita670 <mahajanishu2005@gmail.com>
2025-02-05 02:14:43 +0530

    Solution to Check LSB of a Number

--- Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
+++ Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
@@ -9,3 +9,6 @@ int main(){
         printf("Not Set");
     }
 }
+//lsb is 1 if number is odd and 0 if number is even
+//lsb is the last digit in the binary representation 
+//1 will come at last only in the case of odd numbers
