cdead84a551e0395f51e9f82af84ca3d1629620a
Ishita670 <mahajanishu2005@gmail.com>
2025-02-08 01:17:04 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -17,8 +17,13 @@ int main(){
             printf("%d",c);
             break;
          case '/':
-            c=a/b;
-            printf("%d",c);
+            if(b!=0){
+                c=a/b;
+                printf("%d",c);
+            }
+            else{
+                printf("error");
+            }
             break;
         default:
             printf("error");
