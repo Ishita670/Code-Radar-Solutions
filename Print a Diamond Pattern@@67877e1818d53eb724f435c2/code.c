f01a5d650e3dceef4fb94859bc6ee6aabba69ab4
Ishita Mahajan <mahajanishu2005@gmail.com>
2025-04-01 10:33:02 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 int main(){
     int n;
-    int i,j,k,x,y,z;
-    scanf("%d",&n);
-    for(i=1; i<=n; i++){
-        for(j=1; j<=n-i; j++){
+    sacnf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1; j<=n-i;j++){
             printf(" ");
         }
-        for(k=1;k<=n;k++){
-            printf("*");
-        }
-        printf("\n");
-    }
-    for(x=n-1;x>=1;x--){
-        for(y=1; y<=n-x; y++){
-            printf(" ");
-        }
-        for(z=n-1; z>=1; z--){
+        for(int k=1; k<i*2; k++){
             printf("*");
         }
         printf("\n");
